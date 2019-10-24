8599721dc6819f7872ca6d618cce0941e8cb3e18
Quinn <qcasey1@gmail.com>
2019-10-23 17:04:34 -0700

    update to latest core

--- main.go
+++ main.go
@@ -3,14 +3,17 @@ package main
 import (
 	"encoding/json"
 	"flag"
-	"log"
 	"net/http"
+	"os"
 	"os/exec"
+	"strconv"
+	"strings"
 
 	"github.com/MrDoctorKovacic/MDroid-Core/bluetooth"
-	"github.com/MrDoctorKovacic/MDroid-Core/logging"
 	"github.com/MrDoctorKovacic/MDroid-Core/settings"
 	"github.com/gorilla/mux"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
 )
 
 // Config controls program settings and general persistent settings
@@ -18,47 +21,47 @@ type Config struct {
 	BluetoothAddress string
 }
 
-// MainStatus will control logging and reporting of status / warnings / errors
-var MainStatus = logging.NewStatus("Main")
-
 // Reboot the machine
 func reboot(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode("OK")
 	exec.Command("reboot", "now")
 }
 
+func init() {
+	zerolog.SetGlobalLevel(zerolog.InfoLevel)
+	zerolog.CallerMarshalFunc = func(file string, line int) string {
+		fileparts := strings.Split(file, "/")
+		filename := strings.Replace(fileparts[len(fileparts)-1], ".go", "", -1)
+		return filename + ":" + strconv.Itoa(line)
+	}
+	output := zerolog.ConsoleWriter{Out: os.Stderr, TimeFormat: "Mon Jan 2 15:04:05"}
+	log.Logger = zerolog.New(output).With().Caller().Timestamp().Logger()
+}
+
 // define our router and subsequent routes here
 func main() {
-
-	// Start with program arguments
-	var (
-		settingsFile string
-	)
-	flag.StringVar(&settingsFile, "settings-file", "", "File to recover the persistent settings.")
+	flag.StringVar(&settings.Settings.File, "settings-file", "", "File to recover the persistent settings.")
+	debug := flag.Bool("debug", false, "sets log level to debug")
 	flag.Parse()
 
-	// Parse settings file
-	settingsData, _ := settings.SetupSettings(settingsFile)
+	if *debug {
+		zerolog.SetGlobalLevel(zerolog.DebugLevel)
+	}
 
-	// Log settings
-	out, err := json.Marshal(settingsData)
-	if err != nil {
+	// Parse settings file
+	settings.ReadFile(settings.Settings.File)
+
+	// Check settings
+	if _, err := json.Marshal(settings.GetAll()); err != nil {
 		panic(err)
 	}
-	MainStatus.Log(logging.OK(), "Using settings: "+string(out))
 
 	// Parse through config if found in settings file
-	config, ok := settingsData["MDROID"]
-	if ok {
-		// Set up bluetooth
-		bluetoothAddress, usingBluetooth := config["BLUETOOTH_ADDRESS"]
-		if usingBluetooth {
-			bluetooth.EnableAutoRefresh()
-			bluetooth.SetAddress(bluetoothAddress)
-		}
-	} else {
-		MainStatus.Log(logging.Warning(), "No config found in settings file, not parsing through config")
+	configMap, err := settings.GetComponent("MDROID")
+	if err != nil {
+		log.Warn().Msg("No config found in settings file, not parsing through config")
 	}
+	bluetooth.Setup(&configMap)
 
 	// Init router
 	router := mux.NewRouter()
@@ -67,14 +70,6 @@ func main() {
 	// Main routes
 	//
 	router.HandleFunc("/restart", reboot).Methods("GET")
-
-	//
-	// Settings routes
-	//
-	router.HandleFunc("/settings", settings.GetAllSettings).Methods("GET")
-	router.HandleFunc("/settings/{component}", settings.GetSetting).Methods("GET")
-	router.HandleFunc("/settings/{component}/{name}", settings.GetSettingValue).Methods("GET")
-	router.HandleFunc("/settings/{component}/{name}/{value}", settings.SetSettingValue).Methods("POST")
 
 	//
 	// Bluetooth routes
@@ -90,5 +85,6 @@ func main() {
 	router.HandleFunc("/bluetooth/play", bluetooth.Play).Methods("GET")
 	router.HandleFunc("/bluetooth/refresh", bluetooth.ForceRefresh).Methods("GET")
 
-	log.Fatal(http.ListenAndServe(":5353", router))
+	err = http.ListenAndServe(":5353", router)
+	log.Error().Msg(err.Error())
 }
